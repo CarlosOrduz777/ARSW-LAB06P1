90e334e37a02c919d9ed11090a4264adf64cbacb
unknown <carlos.orduz@labinfo.is.escuelaing.edu.co>
2022-09-28 09:24:41 -0500

    paint differents blueprints

--- src/main/resources/public/js/apimock.js
+++ src/main/resources/public/js/apimock.js
@@ -1,9 +1,8 @@
-
 apimock=(function(){
 
 	var mockdata=[];
 
-	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
+	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":150,"y":200},{"x":200,"y":200},{"x":200,"y":120},{"x":150,"y":120}],"name":"house"},
 	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
 	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
 	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,6 +1,7 @@
 var app = (function(){
     var author;
     var blueprints;
+    var blueprintsByNameAndAuthor;
 
     var getSelectedAuthor = function(){
         return $("#author").val()
@@ -9,7 +10,16 @@ var app = (function(){
     var getBlueprints = function(list){
         blueprints = list;
     }
+    var getBlueprintsByNameAndAuthor = function(object){
+        blueprintsByNameAndAuthor = object;
+    }
+    function searchBlueprintPoints(authorName, blueprintName){
+            apimock.getBlueprintsByNameAndAuthor(authorName,blueprintName,getBlueprintsByNameAndAuthor);
 
+            var blueprintPoints = blueprintsByNameAndAuthor.points;
+            console.log(blueprintPoints);
+            draw(blueprintPoints);
+    }
     function getAuthorBlueprints(){
         author = getSelectedAuthor();
         console.log(author);
@@ -19,7 +29,7 @@ var app = (function(){
         var blueprintMapped = blueprintsCopy.map((bp) => {return {name:bp.name, pointsNumber:bp.points.length}
         });
         var blueprintAuthorTable = blueprintMapped.map(function(bpm){
-            var row = "<tr><td align=\"center\" id=\""+bpm.name+"_\">"+bpm.name+"</td><td align=\"center\">"+bpm.pointsNumber+"</td></tr>"
+            var row = "<tr><td align=\"center\" id=\""+bpm.name+"_\">"+bpm.name+"</td><td align=\"center\">"+bpm.pointsNumber+"</td><td align=\"center\">"+"<button onclick=\"app.searchBlueprintPoints('"+author+"','"+bpm.name+"')\" type='button' class='btn btn-primary'>Open</button>"+"</td></tr>"
             $("#author-bpTable tbody").append(row);
             return row;
         });
@@ -31,11 +41,30 @@ var app = (function(){
         $("#totalAuthorPoints").text(totalAuthorPoints);
 
     }
+
+
+    function draw(blueprintPoints){
+        console.log("ENTROOO");
+        var canvas = document.getElementById('blueprintCanvas');
+        canvas.width = canvas.width;
+        var ctx = canvas.getContext("2d");
+
+        for(var i=0; i<blueprintPoints.length-1; i++){
+            ctx.moveTo(blueprintPoints[i].x,blueprintPoints[i].y);
+            ctx.lineTo(blueprintPoints[i+1].x,blueprintPoints[i+1].y);
+            ctx.stroke();
+        }
+
+    }
+
+
+
     return{
         getAuthor : function(){
             getSelectedAuthor();
         },
-        getAuthorBlueprints: getAuthorBlueprints
+        getAuthorBlueprints: getAuthorBlueprints,
+        searchBlueprintPoints: searchBlueprintPoints
 
 
     }
